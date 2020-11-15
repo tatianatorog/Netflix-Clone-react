e360197fe24f396e8fa5130156c7293702027c9d
TATIANA Toro <tatianatorog@gmail.com>
2020-11-14 22:22:34 -0500

    Adding trailers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import Row from './Row'
-import requests from './requests'
+import "./App.css";
+import NavBar from "./Navbar";
+import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./requests";
+
 
 function App() {
   return (
     <div className="App">
-    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-    <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <NavBar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top rate" fetchUrl={requests.fetchTopRated} />
+      <Row title="Top rate" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- my-app/src/Row.js
+++ my-app/src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from './axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original"
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl,isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
    useEffect(()=>{
     async function fetchData(){
@@ -16,12 +19,36 @@ function Row({ title, fetchUrl }) {
     fetchData()
    },[fetchUrl])
 
+   const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 0,
+    }
+  }
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.title || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams, urlParams.get('v') )
+          setTrailerUrl(urlParams.get('v'));
+        }).catch((error) => console.log(error));
+    }
+  }
+
+  /* The URLSearchParams API provides a consistent interface to the bits and pieces of the URL 
+  and allows trivial manipulation of the query string (that stuff after "?"). */
+
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
       {movies.map(movie => {
-        return <img key={movie.id} className="row__poster"src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+        return <img key={movie.id} className={`row__poster ${isLargeRow &&"row__posterLarge"}`}src={`${base_url}${isLargeRow ? movie.poster_path: movie.backdrop_path}`} alt={movie.name}/>
       })}
       </div>
     </div>
